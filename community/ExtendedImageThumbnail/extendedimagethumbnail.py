c77840e25ea90db706162bf5da9df1f9cbf877e8
Jez Tucker <jtucker@pixitmedia.com>
2018-04-13 12:34:41 +0100

    Update plugin to demonstrate multiple file types

--- community/ExtendedImageThumbnail/extendedimagethumbnail.py
+++ community/ExtendedImageThumbnail/extendedimagethumbnail.py
@@ -4,14 +4,18 @@ from arcapix.search.metadata.plugins.thumbnails import ImageThumbnail
 class ExtendedImageThumbnail(ImageThumbnail):
     """Generate thumbnail and preview for additional image formats.
 
-    By default, the builtin ImageThumbnail plugin only handles jpeg and dpx.
+    By default, the builtin ImageThumbnail plugin only handles JPEG and DPX.
     """
 
     def handles(self, ext=None, mimetype=None):
-        """Add any formats you want to support below."""
+        """Override the base class handler for the image format"""
+        
         if super(ExtendedImageThumbnail, self).handles(ext, mimetype):
             # don't handle formats already handled by builtin plugin
             return False
 
-        # e.g. add support for png
-        return ext == '.png' or mimetype == "image/png"
+        """Add any additional image formats to support"""
+        SUPPORTED_EXT =  ('.png', '.rgb')
+        SUPPORTED_MIME = ('image/png', 'image/rgb', , 'image/x-rgb')
+        
+        return ext in SUPPORTED_EXT or mimetype in SUPPORTED_MIME
