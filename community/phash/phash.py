d12a7286d0c482605fde581456439e19fc9d6ddf
Chris Oates <coates@pixitmedia.com>
2018-09-18 14:20:48 +0100

    docs: update phash documentation
    Fill in missing plugin documentation.
    
    Fix markdown formatting.

--- community/phash/phash.py
+++ community/phash/phash.py
@@ -10,25 +10,25 @@ logger = logging.getLogger(__name__)
 
 class ImageHashPlugin(Plugin):
     """Plugin that calculates the perceptual hash of an image file.
-    
+
     Can be used to identify duplicates.
-    
+
     Unlike md5 hash, this hash is based on the visual content of the image.
-    
+
     For comparing the exact hash, matches will include exact duplicates
     as well as resized versions of an image. It also has a certain tolerance
     for things like gamma levels, artifacts and noise.
-    
+
     Ideally, hashes would be compared by Hamming distance to find
-    similar images - however Elasticsearch does support such functionality. 
+    similar images - however Elasticsearch does support such functionality.
     """
-    
+
     def namespace(self):
         return 'image'
-    
+
     def handles(self, ext=None, mimetype=None):
         return mimetype and mimetype.startswith('image/')
-    
+
     def schema(self):
         return [{
             "name": "phash",
@@ -40,15 +40,14 @@ class ImageHashPlugin(Plugin):
 
     def process(self, id_, file_, fileinfo=None):
         try:
-            
+
             data = {'phash': str(phash(load_image(file_)))}
-            
+
             if Metadata(id_, self).update(data):
                 return PluginStatus.SUCCESS
-            
+
             return PluginStatus.ERRORED
-        
+
         except:
             logger.exception("Error while processing %r (%s)", file_, id_)
             return PluginStatus.FATAL
-
